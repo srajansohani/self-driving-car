bc9af031f6c97227ffb1828136b32dfd0e6ef336
srajanConsultadd <srajan.s@consultadd.com>
2024-01-27 22:40:22 +0530

    Added sensors which can detect if something is in front

--- car.js
+++ car.js
@@ -1,46 +1,80 @@
-
-class Car {
+class Car{
     constructor(x,y,width,height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.controls = new Controls();
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.angle = 0;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
+
+        this.sensor=new Sensor(this);
+        this.controls=new Controls();
     }
-    update(){
+
+    update(roadBorders){
+        this.#move();
+        this.sensor.update(roadBorders);
+    }
+
+    #move(){
         if(this.controls.forward){
-            this.speed = this.speed + this.acceleration
+            this.speed+=this.acceleration;
         }
         if(this.controls.reverse){
-            this.speed = this.speed - this.acceleration
+            this.speed-=this.acceleration;
         }
-        if(this.controls.left){
-            this.angle = this.angle + 0.02; 
-        }
-        if(this.controls.right){
-            this.angle = this.angle - 0.02;
-        }
-        if(this.speed > this.maxSpeed ){
-            this.speed = this.maxSpeed
-        }
-        this.y = this.y - Math.cos(this.angle)*this.speed;
-        this.x = this.x - Math.sin(this.angle)*this.speed;
-        if(this.speed <  -1 * this.maxSpeed){
-            this.speed = -1 * this.maxSpeed
-        }
-        
 
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
+
     draw(ctx){
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(-this.angle);
+
         ctx.beginPath();
-        ctx.rect(- this.width/2, - this.height/2,this.width,this.height);
-        ctx.fill()
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+
+        ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
